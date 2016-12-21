a886d06b3bb0b937c5367ed40cf8fe9f504b8484
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2016-12-22 03:56:30 +0400

    First one-liner

---
+++ src/XmlShortcuts/Extensions/ParserExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Xml.Linq;
+
+namespace XmlShortcuts.Extensions
+{
+    public static class ParserExtensions
+    {
+        public static string ValueOf(this XElement x, string name) => x?.Element(name)?.Value;
+    }
+}
