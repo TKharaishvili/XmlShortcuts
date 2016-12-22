c85fcd624630de247a7674ddf91a3a67be75b6de
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2016-12-22 04:42:34 +0400

    Tests for string value extracting methods

--- test/XmlShortcuts.test/StringValueParserTests.cs
+++ test/XmlShortcuts.test/StringValueParserTests.cs
@@ -9,8 +9,29 @@ namespace XmlShortcuts.test
         [Fact]
         public void InvokingOnNullReturnsNull()
         {
-            var value = default(XElement).ValueOf("test");
+            var value = default(XElement).ValueOf("node");
             Assert.Null(value);
+        }
+
+        [Fact]
+        public void ValueOfNonExistentElementReturnsNull()
+        {
+            var x = new XElement("root");
+            Assert.Null(x.ValueOf("node"));
+        }
+
+        [Fact]
+        public void ValueOfEmptyElementReturnsEmptyString()
+        {
+            var x = new XElement("root", new XElement("node"));
+            Assert.Equal(x.ValueOf("node"), "");
+        }
+
+        [Fact]
+        public void ExtractsValueCorrectly()
+        {
+            var x = new XElement("root", new XElement("node", "some kind of string"));
+            Assert.Equal(x.ValueOf("node"), "some kind of string");
         }
     }
 }
