a603c78b47c9fc613a0412763e2d540703cc6df0
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2016-12-22 04:27:59 +0400

    xUnit testing set up

---
+++ test/XmlShortcuts.test/StringValueParserTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+using XmlShortcuts.Extensions;
+using Xunit;
+
+namespace XmlShortcuts.test
+{
+    public class StringValueParserTests
+    {
+        [Fact]
+        public void InvokingOnNullReturnsNull()
+        {
+            var value = default(XElement).ValueOf("test");
+            Assert.Null(value);
+        }
+    }
+}
