4a8dca01a133049da8eff059e3ca41894faaeee6
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2017-01-17 18:32:29 +0400

    Long value extracting methods

--- src/XmlShortcuts/Extensions/ParserExtensions.cs
+++ src/XmlShortcuts/Extensions/ParserExtensions.cs
@@ -1,9 +1,23 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Globalization;
+using System.Xml.Linq;
 
 namespace XmlShortcuts.Extensions
 {
     public static class ParserExtensions
     {
         public static string ValueOf(this XElement x, string name) => x?.Element(name)?.Value;
+
+        public static long? LongValueOf(this XElement x, string name)
+        {
+            long result;
+            return long.TryParse(x.ValueOf(name), out result) ? result : default(long?);
+        }
+
+        public static long? LongValueOf(this XElement x, string name, NumberStyles style, IFormatProvider provider)
+        {
+            long result;
+            return long.TryParse(x.ValueOf(name), style, provider, out result) ? result : default(long?);
+        }
     }
 }
